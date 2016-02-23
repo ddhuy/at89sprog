97787f3820ee7c18d1d4182561ef16425c58a8c3
ddhuy <ddhuy137@yahoo.com>
2016-02-23 22:32:09 +0700

    completed renew hexio

--- src/commander/hexio.c
+++ src/commander/hexio.c
@@ -3,12 +3,19 @@
  *      HEADERS
  *
  ******************************************************************/
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <errno.h>
+#include <stdio.h>
+#include <malloc.h>
 
-#include "usbserial.h"
+#include "hexio.h"
+
+
+/*******************************************************************
+ *
+ *      PRIVATE FUNCTION DECLARATION
+ *
+ ******************************************************************/
+static int
+fsize ( FILE* pfile );
 
 
 /*******************************************************************
@@ -18,239 +25,78 @@
  ******************************************************************/
 /*
  * Purpose:
- * - Open connection to Usb Serial device
  *
  * Input:
- * - [IN]  dev_name: name of device (e.g.: /dev/ttyACM0)
- * - [OUT] dev_ptr: pointer to UsbSerialDevice, used to store its termios
- * - [IN]  baudrate: serial data bit rate
- * - [IN]  config: data bit, parity, stop bits configurations
  *
  * Output:
- * - Return OK, if connection is opened successfully, else return error code
+ *
  */
 AT89S_EID
-usbserial_open ( char* dev_name,
-                 UsbSerialDevice* dev_ptr,
-                 int baudrate,
-                 int config )
+hexio_readall ( char* filename,
+                unsigned char* data_ptr,
+                int* data_len_ptr )
 {
-    speed_t io_baudrate;
+    FILE* pfile = NULL;
+    int ipos = 0;
 
-    if (dev_name == NULL || dev_ptr == NULL)
+    if (filename == NULL
+        || data_len_ptr == NULL)
     {
         return AT89S_EID_ARG_NULL;
     }
 
-    if (baudrate != US_BAUDRATE_9600
-        || baudrate != US_BAUDRATE_19200
-        || baudrate != US_BAUDRATE_115200)
+    // open file
+    pfile = fopen(filename, "rb");
+    if (pfile == NULL)
     {
-        return AT89S_EID_SERIAL_INVALID_BAUDRATE;
+        return AT89S_EID_HEXIO_OPEN;
     }
 
-    if (config != US_CONFIG_8N1
-        || config != US_CONFIG_7E1
-        || config != US_CONFIG_7O1)
+    // get file length
+    *data_len_ptr = fsize(pfile);
+    if (*data_len_ptr < 0)
     {
-        return AT89S_EID_SERIAL_INVALID_CONFIG;
+        fclose(pfile);
+        return AT89S_EID_HEXIO_LENGTH;
     }
 
-    // store device name
-    strcpy(dev_ptr->name, dev_name);
-
-    // open device
-    dev_ptr->fd = open (dev_ptr->name,
-                        O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
-    if (dev_ptr->fd < 0)
+    // allocate enough memory to store whole file
+    data_ptr = (unsigned char*) malloc(*data_len_ptr);
+    if (data_ptr == NULL)
     {
-        return AT89S_EID_SERIAL_OPEN;
+        fclose(pfile);
+        return AT89S_EID_HEXIO_SMALL_BUFFER;
     }
 
-    // get current termios settings
-    if (tcgetattr(dev_ptr->fd, &dev_ptr->tios) < 0)
+    // read whole file to new allocated memory
+    ipos = fread(data_ptr, 1, *data_len_ptr, pfile);
+    if (ipos <= 0)
     {
-        return AT89S_EID_SERIAL_GET_ATTR;
+        fclose(pfile);
+        return AT89S_EID_HEXIO_READALL;
     }
 
-    // set input/output baudrdate
-    if (baudrate == US_BAUDRATE_9600)
-        io_baudrate = B9600;
-    else if (baudrate == US_BAUDRATE_19200)
-        io_baudrate = B19200;
-    else if (baudrate == US_BAUDRATE_115200)
-        io_baudrate = B115200;
-
-    if (cfsetispeed(&dev_ptr->tios, baudrate) != 0
-        || cfsetospeed(&dev_ptr->tios, baudrate) != 0)
-    {
-        return AT89S_EID_SERIAL_SET_IOSPEED;
-    }
-
-    // enable receiver, ignore status line
-    dev_ptr->tios.c_cflag |= (CREAD | CLOCAL);
-    // set config
-    if (config == US_CONFIG_8N1)
-    {
-        dev_ptr->tios.c_cflag &= ~PARENB;
-        dev_ptr->tios.c_cflag &= ~CSTOPB;
-        dev_ptr->tios.c_cflag &= ~CSIZE;
-        dev_ptr->tios.c_cflag |= CS8;
-    }
-    else if (config == US_CONFIG_7E1)
-    {
-        dev_ptr->tios.c_cflag |= PARENB;
-        dev_ptr->tios.c_cflag &= ~PARODD;
-        dev_ptr->tios.c_cflag &= ~CSTOPB;
-        dev_ptr->tios.c_cflag &= ~CSIZE;
-        dev_ptr->tios.c_cflag |= CS7;
-    }
-    else if (config == US_CONFIG_7O1)
-    {
-        dev_ptr->tios.c_cflag |= PARENB;
-        dev_ptr->tios.c_cflag |= PARODD;
-        dev_ptr->tios.c_cflag &= ~CSTOPB;
-        dev_ptr->tios.c_cflag &= ~CSIZE;
-        dev_ptr->tios.c_cflag |= CS7;
-    }
-
-    // no HW flow control
-    dev_ptr->tios.c_cflag &= ~CRTSCTS;
-
-    // no input processing (raw input)
-    dev_ptr->tios.c_iflag &= ~(IXON | IXOFF | IXANY);
-    // other input settings
-    dev_ptr->tios.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-    // no output processing (raw output)
-    dev_ptr->tios.c_oflag &= ~OPOST;
-
-    // control character settings
-    dev_ptr->tios.c_cc[VMIN]  = 1; // wait for 1 minimum chacacter received
-    dev_ptr->tios.c_cc[VTIME] = 0; // no timeout when waiting for charater
-
-    // commit new settings
-    if (tcsetattr(dev_ptr->fd, TCSANOW, &dev_ptr->tios) < 0)
-    {
-        return AT89S_EID_SERIAL_SET_ATTR;
-    }
-
-    // wait for device reset & sync up
-    usleep(1500 * 1000);
-
+    fclose(pfile);
     return AT89S_EID_OK;
-} /* usbserial_open */
-
+}
 
 /*
  * Purpose:
- * - Close an opened connection to Usb Serial device
- *
- * Input:
- * - [OUT] dev_ptr: pointer to UsbSerialDevice, used to store its termios
- *
- * Output:
- * - Return OK, if connection is closed successfully, else return error code
+ * - Return size of file in byte
  */
-AT89S_EID
-usbserial_close ( UsbSerialDevice* dev_ptr )
+static int
+fsize ( FILE* pfile )
 {
-    if (dev_ptr == NULL)
-    {
-        return AT89S_EID_ARG_NULL;
-    }
+    int prev_pos = -1;
+    int file_size = -1;
 
-    if (close(dev_ptr->fd) < 0)
-    {
-        return AT89S_EID_SERIAL_CLOSE;
-    }
+    if (pfile == NULL)
+        return -1;
 
-    return AT89S_EID_OK;
-} /* usbserial_close */
+    prev_pos = ftell(pfile);
+    fseek(pfile, 0L, SEEK_END);
+    file_size = ftell(pfile);
+    fseek(pfile, prev_pos, SEEK_SET);
 
-
-/*
- * Purpose:
- *
- * Input:
- *
- * Output:
- */
-AT89S_EID
-usbserial_send ( UsbSerialDevice* dev_ptr,
-                 unsigned char* data_ptr,
-                 int data_len )
-{
-    int byte_sent = 0;
-
-    if (dev_ptr == NULL || data_ptr == NULL)
-    {
-        return AT89S_EID_ARG_NULL;
-    }
-
-    // send data
-    while (byte_sent < data_len)
-    {
-        byte_sent = write(dev_ptr->fd, data_ptr, data_len);
-        if (byte_sent > 0)
-        {
-            data_ptr += byte_sent;
-            data_len -= byte_sent;
-        }
-        else
-        {
-            if (errno == EAGAIN)
-                continue;
-            else
-                return AT89S_EID_SERIAL_SEND;
-        }
-    }
-
-    return AT89S_EID_OK;
-} /* usbserial_send */
-
-
-/*
- * Purpose:
- *
- * Input:
- *
- * Output:
- */
-AT89S_EID
-usbserial_recv ( UsbSerialDevice* dev_ptr,
-                 unsigned char* data_ptr,
-                 int data_len )
-{
-    int read_byte = 0;
-    char b[1];
-
-    if (dev_ptr == NULL
-        || data_ptr == NULL)
-    {
-        return AT89S_EID_ARG_NULL;
-    }
-
-    // block reading
-    fcntl(dev_ptr->fd, F_SETFL, 0);
-
-    // start receiving data
-    while (read_byte < data_len)
-    {
-        if (read(dev_ptr->fd, b, 1) > 0)
-        {
-            data_ptr[read_byte++] = *b;
-        }
-        else
-        {
-            if (errno == EAGAIN)
-                continue;
-            else if (errno == ETIMEDOUT)
-                break;
-            else
-                return AT89S_EID_SERIAL_RECV;
-        }
-    }
-
-    return AT89S_EID_OK;
-} /* usbserial_recv */
+    return file_size;
+}
--- src/commander/hexio.h
+++ src/commander/hexio.h
@@ -1,47 +1,13 @@
-#ifndef _USB_SERIAL_H_
-#define _USB_SERIAL_H_
+#ifndef _HEX_IO_H_
+#define _HEX_IO_H_
 
 /*******************************************************************
  *
  *      HEADERS
  *
  ******************************************************************/
-#include <termios.h>
 
 #include "at89s_types.h"
-
-/*******************************************************************
- *
- *      PREPROCESSOR & CONSTANT
- *
- ******************************************************************/
-// Serial Baudrate
-#define  US_BAUDRATE_9600      9600
-#define  US_BAUDRATE_19200     19200
-#define  US_BAUDRATE_115200    115200
-
-// 8N1
-#define  US_CONFIG_8N1   0x0001
-// 7E1
-#define  US_CONFIG_7E1   0x0002
-// 7O1
-#define  US_CONFIG_7O1   0x0003
-
-// Maximum characters of device name
-#define  NAME_MAX    256
-
-/*******************************************************************
- *
- *      DATA TYPE
- *
- ******************************************************************/
-typedef struct UsbSerialDevice_t
-{
-    int fd;
-    char name[NAME_MAX];
-    struct termios tios;
-
-} UsbSerialDevice;
 
 
 
@@ -54,32 +20,9 @@ typedef struct UsbSerialDevice_t
  *
  */
 AT89S_EID
-usbserial_open ( char* dev_name,
-                 UsbSerialDevice* dev_ptr,
-                 int baudrate,
-                 int config );
-
-/*
- *
- */
-AT89S_EID
-usbserial_send ( UsbSerialDevice* dev_ptr,
-                 unsigned char* data_ptr,
-                 int data_len );
-
-/*
- *
- */
-AT89S_EID
-usbserial_recv ( UsbSerialDevice* dev_ptr,
-                 unsigned char* data_ptr,
-                 int data_len );
-
-/*
- *
- */
-AT89S_EID
-usbserial_close ( UsbSerialDevice* dev_ptr );
+hexio_readall ( char* filename,
+                unsigned char* data_ptr,
+                int* data_len_ptr );
 
 
-#endif /* _USB_SERIAL_H_ */
+#endif /* _HEX_IO_H_ */
